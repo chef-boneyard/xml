3ee92da5f941d42e6a1964e2c71d796f5f32439b
jtimberman <joshua@opscode.com>
2012-12-11 17:39:01 -0700

    release v1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs xml"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.5"
+version          "1.1.0"
 
 recipe "xml", "Installs libxml development packages"
 
