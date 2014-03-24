219fdb795df7060a2619ff6b3e02cb954631d301
Xabier de Zuazo <xabier@onddo.com>
2014-03-24 14:17:01 +0100

    [COOK-4468] Use ["nokogiri"]["use_system_libraries"] attribute #to_s instead of an if condition

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -30,6 +30,6 @@ include_recipe 'build-essential::default'
 include_recipe 'xml::default'
 
 # See https://github.com/sparklemotion/nokogiri/blob/master/CHANGELOG.rdoc#160rc1--2013-04-14
-ENV['NOKOGIRI_USE_SYSTEM_LIBRARIES'] = 'true' if node['xml']['nokogiri']['use_system_libraries']
+ENV['NOKOGIRI_USE_SYSTEM_LIBRARIES'] = node['xml']['nokogiri']['use_system_libraries'].to_s
 
 chef_gem 'nokogiri'
