b743b3b3c19eb05f8b7f3a8e7956aff8091eaf38
Tim Smith <tsmith84@gmail.com>
2015-12-07 13:48:24 -0800

    Fix failing spec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'xml::default' do
-  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
 
-  it 'installs the XML package' do
-    expect(chef_run).to install_package('libxml2-dev')
-    expect(chef_run).to install_package('libxslt-dev')
-    expect(chef_run).to install_package('zlib1g-dev')
+  it 'installs the XML packages during the converge phase' do
+    expect(chef_run).to install_package('libxml2-dev').at_converge_time
+    expect(chef_run).to install_package('libxslt-dev').at_converge_time
+    expect(chef_run).to install_package('zlib1g-dev').at_converge_time
   end
 end
