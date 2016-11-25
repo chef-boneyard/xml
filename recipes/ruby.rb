bff2ebe1a01af0158f73ec9dbbb420f43b249141
Tim Smith <tsmith@chef.io>
2016-11-25 15:01:07 -0800

    Update copyright headers
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,10 +1,10 @@
 #
-# Cookbook Name:: xml
+# Cookbook:: xml
 # Recipe:: ruby
 #
 # Author:: Joseph Holsten (<joseph@josephholsten.com>)
 #
-# Copyright 2008-2016, Chef Software, Inc.
+# Copyright:: 2008-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
